3b31cfef48c5bc5a7a06d7c5fbaee42c5b273521
liam2292 <liamakpata@gmail.com>
2023-06-19 13:31:19 -0700

    linked buttons

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -13,4 +13,35 @@ export class NavbarComponent {
   toggleContent(): void {
     this.isMenuHidden = !this.isMenuHidden;
   }
+
+  scrollToHeader() {
+    const header = document.getElementById('header');
+    if (header) {
+      header.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  scrollToHeaderAndClose() {
+    const header = document.getElementById('header');
+    if (header) {
+      header.scrollIntoView({ behavior: 'smooth' });
+    }
+    this.isMenuHidden = !this.isMenuHidden;
+  }
+
+  scrollToContactInfoAndClose() {
+    const contactInfo = document.getElementById('contact-info');
+    if (contactInfo) {
+      contactInfo.scrollIntoView({ behavior: 'smooth' });
+    }
+    this.isMenuHidden = !this.isMenuHidden;
+  }
+
+  scrollToSocialsAndClose() {
+    const socialMedia = document.getElementById('social-media');
+    if (socialMedia) {
+      socialMedia.scrollIntoView({ behavior: 'smooth' });
+    }
+    this.isMenuHidden = !this.isMenuHidden;
+  }
 }
